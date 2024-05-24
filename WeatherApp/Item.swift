321147deb0099b18ef9762bcff97731036018fd4
Lucas Gabriel <lucasgabrieltais16@gmail.com>
2024-05-24 16:10:30 -0300

    Initial Commit

---
+++ WeatherApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  WeatherApp
+//
+//  Created by Lucas Gabriel Tais on 24/05/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
